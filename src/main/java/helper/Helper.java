f33e43d43e1ad5317c6cc48e40af6d0b1302c6ce
Vladimir Bojcic <bojcicv@gmail.com>
2023-10-25 04:53:56 +0200

    Add Helper class and Update .gitignore

---
+++ src/main/java/helper/Helper.java
@@ -0,0 +1,16 @@
+package helper;
+
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Helper {
+    public static void takeAScreenshot(WebDriver driver, String path) throws IOException {
+        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        Files.copy(screenshot,new File(path));
+    }
+}
